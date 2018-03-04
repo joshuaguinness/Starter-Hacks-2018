b5ff05ce4b870806588bfe81440698dae6442ec4
Tim Choy <timchoy99@gmail.com>
2018-03-03 23:01:55 -0500

    ++ Updated xml files, optimized MainActivity and aboutActivity -- Deleted unused lines of code and constants

--- app/src/main/java/com/nextdev/starterhacks/MainActivity.java
+++ app/src/main/java/com/nextdev/starterhacks/MainActivity.java
@@ -1,18 +1,46 @@
 package com.nextdev.starterhacks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
 import android.view.View;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat gestureObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gestureObject = new GestureDetectorCompat(this, new SwipeGesture());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureObject.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
     public void addMessage (View view) {
         /* this is to activate the add fragment */
+    }
+
+    class SwipeGesture extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            if(event2.getX() < event1.getX()) {
+                Intent intent = new Intent(
+                        MainActivity.this, aboutActivity.class);
+                finish();
+                startActivity(intent);
+            }
+            return true;
+        }
     }
 }
--- app/src/main/java/com/nextdev/starterhacks/aboutActivity.java
+++ app/src/main/java/com/nextdev/starterhacks/aboutActivity.java
@@ -1,13 +1,41 @@
 package com.nextdev.starterhacks;
 
+import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class aboutActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat gestureObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        gestureObject = new GestureDetectorCompat(this, new SwipeGesture2());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureObject.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class SwipeGesture2 extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            if(event2.getX() > event1.getX()) {
+                Intent intent = new Intent(
+                        aboutActivity.this, MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+            return true;
+        }
     }
 }
