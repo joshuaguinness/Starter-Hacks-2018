b9c8055a4c14b08f5bc14f93a20d1a5567ff9803
Timothy Elgersma <Timothy.elgersma@gmail.com>
2018-03-04 01:27:26 -0500

    Added print to IO

--- app/src/main/java/com/nextdev/starterhacks/fileIO.java
+++ app/src/main/java/com/nextdev/starterhacks/fileIO.java
@@ -1,7 +1,18 @@
 package com.nextdev.starterhacks;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -9,7 +20,8 @@ import java.util.ArrayList;
  */
 
 public class fileIO {
-    public static void main(String [] args) {
+    ArrayList<location> readIn() {
+
         ArrayList<location> myList = new ArrayList<location>();
 
         String file = "pastLocations.txt";
@@ -24,6 +36,21 @@ public class fileIO {
             }
         } catch(Exception e) {
             e.printStackTrace();
+        }
+        return myList;
+    }
+    void printOut(ArrayList<location> myList) {
+        try {
+        File file = new File("pastLocations.txt"); //will overwrite
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (int i = 0; i < myList.size(); i++) {
+            String s = myList.get(i).addr + "," + Integer.toString(myList.get(i).haz) + ',' + myList.get(i).desc;
+            bw.write(s);
+        }
+        bw.close();
+    } catch(Exception e) {
+            System.out.println(e);
         }
     }
 }
