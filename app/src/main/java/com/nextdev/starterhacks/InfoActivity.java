54232afff90e5c3566d37f9e1ad68a96389faf3c
Joshua Guinness <joshua.guinness@gmail.com>
2018-03-04 13:17:29 -0500

    Added a google maps fragment

--- app/src/main/java/com/nextdev/starterhacks/InfoActivity.java
+++ app/src/main/java/com/nextdev/starterhacks/InfoActivity.java
@@ -4,14 +4,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class InfoActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class InfoActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map
         setContentView(R.layout.activity_info);
-
+        //Get the SupportMapFragment and request notification
+        //when the map is read to be used
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         //Get the Intent that started this activity
         Intent intent = getIntent();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //Add a marker in Sydney, Australia, and move the map's camera to the same location
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
